836e10e44882ae6c5d2e5defa25c78ee4c3d5afb
raha <elahe.dstn@gmail.com>
2020-07-25 15:20:01 +0430

    setup main

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"checker/balancer"
+	"checker/config"
+	"checker/subscriber"
+)
+
 func main() {
-	
+	cfg := config.Read()
+	s := subscriber.New(balancer.New(cfg.Nats), cfg.Nats)
+
+	s.Subscribe()
 }
